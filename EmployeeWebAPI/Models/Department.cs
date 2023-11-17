7ad7becc929c49ee1f0d432eb463881cd0f2afdb
hadilbj <hadilbenjabra1@gmail.com>
2023-11-18 00:05:50 +0100

    Ajoutez des fichiers projet.

---
+++ EmployeeWebAPI/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeWebAPI.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set;}
+    }
+    
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
